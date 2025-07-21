0d0f603d3a95cb936f17cb3ed79334c793d33f2d
Khanh Nguyen <takashilouisnguyen@gmail.com>
2025-07-21 17:59:59 -0500

    Implemented landlord properties route and enhance LandlordListingService with getAll and delete methods. Added CardListing model for improved listing management.

--- src/app/landlord/landlord-listing.service.ts
+++ src/app/landlord/landlord-listing.service.ts
@@ -1,6 +1,6 @@
 import { computed, inject, Injectable,signal, WritableSignal } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
-import { CreatedListing, NewListing } from "./model/listing.model"
+import { CardListing, CreatedListing, NewListing } from "./model/listing.model"
 import { State } from "../core/model/state.model"
 import { environment } from "../../environments/environment"
 
@@ -8,14 +8,21 @@ import { environment } from "../../environments/environment"
   providedIn: 'root'
 })
 export class LandlordListingService {
-  httpClient = inject(HttpClient)
+  http = inject(HttpClient)
    
   constructor() { }
-
   private create$: WritableSignal<State<CreatedListing>> = 
     signal(State.Builder<CreatedListing>().forInit())
   
   createSig = computed( () => this.create$());
+
+  private getAll$: WritableSignal<State<Array<CardListing>>> =
+    signal(State.Builder<Array<CardListing>>().forInit())
+  getAllSig = computed(() => this.getAll$());
+
+  private delete$: WritableSignal<State<string>> =
+    signal(State.Builder<string>().forInit())
+  deleteSig = computed(() => this.delete$());
 
   create(newListing: NewListing): void {
     const formData = new FormData();
@@ -25,7 +32,7 @@ export class LandlordListingService {
     const clone = structuredClone(newListing);
     clone.pictures = [];
     formData.append("dto", JSON.stringify(clone));
-    this.httpClient.post<CreatedListing>(`${environment.API_URL}/landlord-listing/create`,formData).subscribe({
+    this.http.post<CreatedListing>(`${environment.API_URL}/landlord-listing/create`,formData).subscribe({
       next: listing => this.create$.set(State.Builder<CreatedListing>().forSuccess(listing)),
       error: err => this.create$.set(State.Builder<CreatedListing>().forError(err))
     });
@@ -34,4 +41,27 @@ export class LandlordListingService {
   resetListingCreation(): void {
     this.create$.set(State.Builder<CreatedListing>().forInit())
   }
+
+  getAll(): void {
+    this.http.get<Array<CardListing>>(`${environment.API_URL}/landlord-listing/get-all`)
+      .subscribe({
+        next: listings => this.getAll$.set(State.Builder<Array<CardListing>>().forSuccess(listings)),
+        error: err => this.create$.set(State.Builder<CreatedListing>().forError(err)),
+      });
+  }
+
+  delete(publicId: string): void {
+    const params = new HttpParams().set("publicId", publicId);
+    this.http.delete<string>(`${environment.API_URL}/landlord-listing/delete`, {params})
+      .subscribe({
+        next: publicId => this.delete$.set(State.Builder<string>().forSuccess(publicId)),
+        error: err => this.create$.set(State.Builder<CreatedListing>().forError(err)),
+      });
+  }
+
+  resetDelete(): void{
+    this.delete$.set(State.Builder<string>().forInit())
+  }
+
+
 }
---
+++ src/app/tenant/model/booking.model.ts
@@ -0,0 +1,18 @@
+import {DisplayPicture} from "../../landlord/model/listing.model";
+import {PriceVO} from "../../landlord/model/listing-vo.model";
+// import {Dayjs} from "dayjs";
+
+export interface BookedDatesDTOFromServer {
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface BookedListing {
+    location: string,
+    cover: DisplayPicture,
+    totalPrice: PriceVO,
+    dates: BookedDatesDTOFromServer,
+    bookingPublicId: string,
+    listingPublicId: string,
+    loading: boolean
+}
